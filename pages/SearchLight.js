63858be94d3d135d0940dce22150edba10117241
Jakob Ossmann <Jakobossmann@gmail.com>
2024-03-09 13:22:39 +1000

    SearchLight App

---
+++ pages/SearchLight.js
@@ -0,0 +1,14 @@
+// pages/NewSubpage.js
+
+import React from "react";
+
+const NewSubpage = () => {
+  return (
+    <div>
+      <h1>New Subpage</h1>
+      <p>This is the content of the new subpage.</p>
+    </div>
+  );
+};
+
+export default NewSubpage;
