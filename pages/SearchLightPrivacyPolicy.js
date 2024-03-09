a28f4ba463b20960aca9ce93c982677ccecaa156
Jakob Ossmann <Jakobossmann@gmail.com>
2024-03-09 14:24:19 +1000

    Privacy Policy

--- pages/SearchLightPrivacyPolicy.js
+++ pages/SearchLightPrivacyPolicy.js
@@ -1,6 +1,5 @@
 // New Subpage for the Privacy Policy of SearchLight App
 
-import "../styles/searchLight.css"; // Import custom page-specific CSS
 import Image from 'next/image';
 import { ThemeProvider } from "next-themes";
 
@@ -8,24 +7,27 @@ const SearchLightPrivacyPolicy = () => {
     const imageSrc = "/images/SearchLight_icon.jpg"; // Provide the correct path to your image
   
     return (
-      <div>
-        <Image src={imageSrc} alt="App Icon" className="app-icon" />
-        <h3>Privacy Policy</h3>
-        <p>
-          Thank you for considering using the application named &quot;SearchLight&quot;.
-          The app does not collect or access remotely any form of data.
-        </p>
-        <p>
-          Data that is processed to generate customized investment ideas for the user, is stored only on the user&apos;s device and will be automatically deleted when the user chooses to remove the application.
-        </p>
-        <p>
-          Should the app be updated to include any form of data collection, users will be informed and the privacy policy here updated.
-        </p>
-        <p>
-          Responsible for the data of this app is Mr. Jakob Ossmann. Contact via office@softwaresuccess.com.au.
-        </p>
-      </div>
+      <ThemeProvider>
+        <div>
+          <Image src={imageSrc} alt="App Icon" className="app-icon" />
+          <h3>Privacy Policy</h3>
+          <p>
+            Thank you for considering using the application named &quot;SearchLight&quot;.
+            The app does not collect or access remotely any form of data.
+          </p>
+          <p>
+            Data that is processed to generate customized investment ideas for the user, is stored only on the user&apos;s device and will be automatically deleted when the user chooses to remove the application.
+          </p>
+          <p>
+            Should the app be updated to include any form of data collection, users will be informed and the privacy policy here updated.
+          </p>
+          <p>
+            Responsible for the data of this app is Mr. Jakob Ossmann. Contact via office@softwaresuccess.com.au.
+          </p>
+        </div>
+      </ThemeProvider>
     );
 };
 
 export default SearchLightPrivacyPolicy;
+
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import "../styles/globals.css";
+import "../styles/searchLight.css"; // Import custom page-specific CSS
+
 import { ThemeProvider } from "next-themes";
 
 const App = ({ Component, pageProps }) => {
